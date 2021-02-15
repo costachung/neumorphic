c48905fd89a42aa075fa7d6c73ad450cce3f44ea
TheMisfit68 <janverrept@me.com>
2021-02-15 22:12:59 +0100

    Minimized the amount of platform specific code Used SwiftUI-color as the base type again

--- Sources/Neumorphic/ColorExtension.swift
+++ Sources/Neumorphic/ColorExtension.swift
@@ -1,41 +1,47 @@
+
 import SwiftUI
 
 public extension Color {
-	
+
 	struct Neumorphic {
 		
-		public static var colorScheme:ColorScheme = .light
-		
-		private static let defaultMainColor = Color(red: 0.925, green: 0.941, blue: 0.953, opacity: 1.000)
-		private static let defaultSecondaryColor = Color(red: 0.482, green: 0.502, blue: 0.549, opacity: 1.000)
-		private static let defaultLightShadowSolidColor = Color(red: 1.000, green: 1.000, blue: 1.000, opacity: 1.000)
-		private static let defaultdarkShadowSolidColor = Color(red: 0.820, green: 0.851, blue: 0.902, opacity: 1.000)
-		private static let defaultLightShadowColor = Color(red: 1.000, green: 1.000, blue: 1.000, opacity: 0.500)
-		private static let defaultdarkShadowColor = Color(red: 0.66, green: 0.72, blue: 0.84, opacity: 0.55)
-		
-		private static let darkThemeMainColor = Color(red: 0.188, green: 0.192, blue: 0.208, opacity: 1.000)
-		private static let darkThemeSecondaryColor = Color(red: 0.910, green: 0.910, blue: 0.910, opacity: 1.000)
-		private static let darkThemeLightShadowSolidColor = Color(red: 0.243, green: 0.247, blue: 0.275, opacity: 1.000)
-		private static let darkThemedarkShadowSolidColor = Color(red: 0.137, green: 0.137, blue: 0.137, opacity: 1.000)
-		private static let darkThemeLightShadowColor = Color(red: 1, green: 1, blue: 1, opacity: 0.1)
-		private static let darkThemedarkShadowColor = Color(red: 0, green: 0, blue: 0, opacity: 0.5)
+		private static var isDarkMode:Bool {
+			#if os(iOS)
+			return UITraitCollection.current.userInterfaceStyle == .dark
+			#else
+			if let window = NSApp.mainWindow {
+				return window.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+			}
+			return NSAppearance.current.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
+			#endif
+		}
 		
 		
+		//Color
+		public static let defaultMainColor = Color(red: 0.925, green: 0.941, blue: 0.953, opacity: 1.000)
+		public static let defaultSecondaryColor = Color(red: 0.482, green: 0.502, blue: 0.549, opacity: 1.000)
+		public static let defaultLightShadowSolidColor = Color(red: 1.000, green: 1.000, blue: 1.000, opacity: 1.000)
+		public static let defaultDarkShadowSolidColor = Color(red: 0.820, green: 0.851, blue: 0.902, opacity: 1.000)
+
+		public static let darkThemeMainColor = Color(red: 0.188, green: 0.192, blue: 0.208, opacity: 1.000)
+		public static let darkThemeSecondaryColor = Color(red: 0.910, green: 0.910, blue: 0.910, opacity: 1.000)
+		public static let darkThemeLightShadowSolidColor = Color(red: 0.243, green: 0.247, blue: 0.275, opacity: 1.000)
+		public static let darkThemeDarkShadowSolidColor = Color(red: 0.137, green: 0.137, blue: 0.137, opacity: 1.000)
+				
 		public static var main: Color {
-			colorScheme == .dark ? Self.darkThemeMainColor : Self.defaultMainColor
+			isDarkMode ? darkThemeMainColor : defaultMainColor
 		}
 		
 		public static var secondary: Color {
-			colorScheme == .dark ? Self.darkThemeSecondaryColor : Self.defaultSecondaryColor
+			isDarkMode ? darkThemeSecondaryColor : defaultSecondaryColor
 		}
-		
+
 		public static var lightShadow: Color {
-			colorScheme == .dark ? Self.darkThemeLightShadowSolidColor : Self.defaultLightShadowSolidColor
+			isDarkMode ? darkThemeLightShadowSolidColor : defaultLightShadowSolidColor
 		}
-		
+
 		public static var darkShadow: Color {
-			colorScheme == .dark ? Self.darkThemedarkShadowSolidColor : Self.defaultdarkShadowSolidColor
+			isDarkMode ? darkThemeDarkShadowSolidColor : defaultDarkShadowSolidColor
 		}
-		
 	}
 }
